2d9002757921cd59c293f5cd452c2ac50c824386
Anujin <anuka1114@yahoo.com>
2022-10-27 12:28:11 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,21 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2022 Anujin-crs
+#SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+[ "$res" = 0 ] && echo ok
+
+exit $res
+
+
+
 
